b52be365103fcd65a58717f3fc106b61f029c26e
Yeeun102 <164315574+Yeeun102@users.noreply.github.com>
2025-07-21 20:35:37 +0900

    Add Potion

---
+++ SomewhereProject/Assets/CafeGame/Scripts/Node.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // to determine whether the space can be filled with potions or not.
+    public bool isUsable;
+
+    public GameObject potion;
+
+    public Node(bool _isUsable, GameObject _potion)
+    {
+        isUsable = _isUsable;
+        potion = _potion;
+    }
+}
